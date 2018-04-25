db091b45ed9a4fbf950008d4f0404a23eed45216
zigor1980 <zihor1980@gmail.com>
2018-04-25 12:19:43 +0300

    Add static to server.js and set proxy

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const app = express();
 
-// Serve static files from the React app
-
-// Put all API endpoints under '/api'
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/build'));
 
 app.get('*', function(req, res){
-  res.sendfile(__dirname + '/public/index.html');
+  res.sendfile(__dirname + '/build/index.html');
 });
 
 const port = process.env.PORT || 5000;
 app.listen(port);
-
-console.log(`Password generator listening on ${port}`);
--- src/actions/fetchImages.js
+++ src/actions/fetchImages.js
@@ -2,7 +2,7 @@ export default function fetchImages() {
     return async function (dispatch, getState) {
         try {
             let offset = getState().images.offset;
-            let response = await fetch(`http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=07tdqxOlkiUUHsHsIhyupUWo3nsUqmE1&offset=${offset}`);
+            let response = await fetch(`https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=07tdqxOlkiUUHsHsIhyupUWo3nsUqmE1&offset=${offset}`);
             let json = await response.json();
             console.log(json);
             const { data, pagination } = json;
